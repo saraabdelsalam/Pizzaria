14c2d8a37cfbcc133da4ef99f3faa77b971c792e
sara abdelsalam <saraabdelsalam.41@gmail.com>
2022-08-07 16:05:10 +0200

    updates

--- db_tables.py
+++ db_tables.py
@@ -1,6 +1,23 @@
 from sqlite3 import connect
 from db import get_db,close_db
 
+
+def Make_Order() :
+    connect_db = get_db()
+    try:
+      connect_db.execute("""CREATE TABLE orders (
+                        name TEXT UNIQUE NOT NULL, 
+                        size TEXT NOT NULL,
+                        amount TEXT
+                       );""")
+    except:
+      print("table orders already exists")
+      connect_db.commit()
+      close_db(connect_db)
+
+Make_Order()
+
+
 def create_tables():
   connect_db=get_db()
   try:
@@ -17,39 +34,3 @@ def create_tables():
     close_db(connect_db)
 
 create_tables()
-
-def create_tables2():
-  connect_db=get_db()
-  try:
-      connect_db.execute("""CREATE TABLE order(
-        meal TEXT NOT NULL,
-       address TEXT NOT NULL,
-       amount INTEGER
-   );""")
-
-  except:
-    print("table 2 already exists")
-    connect_db.commit()
-    close_db(connect_db)
-
-create_tables2()
-
-
-def create_tables3():
-  connect_db=get_db()
-  try:
-      connect_db.execute("""CREATE TABLE menu(
-        pizza_name TEXT NOT NULL,
-        size TEXT,
-        price INTEGER,
-
-   );""")
-
-  except:
-    print("table 3 already exists")
-    connect_db.commit()
-    close_db(connect_db)
-
-create_tables3()
-
-  
--- pizzaria.py
+++ pizzaria.py
@@ -3,20 +3,28 @@ from unittest import result
 from flask import redirect, request, url_for
 from flask import Flask
 from flask import render_template
-import db
+import db 
 
 app= Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home_page():
 
     return render_template('main.html')
 
 
-@app.route('/Register')
+@app.route('/Register', methods=['GET','POST'])
 def Register():
-
-   return render_template('register.html')    
+    if request.method=="POST":
+        name = request.form['name']
+        email= request.form['email']
+        password= request.form['password']
+        address= request.form['address']
+        connect= db.get_db()
+        connect.execute(f"""INSERT INTO users(name,email,password,address)VALUES('{name}', '{email}', '{password}','{address}');""")
+        connect.commit()
+        db.close_db(connect)
+    return render_template('register.html')    
 
 @app.route('/menu',methods=['GET', 'POST'])
 def menu():
@@ -34,9 +42,18 @@ def menu():
     #return render_template('menu.html', data=data)
     return render_template('menu.html')
 
-@app.route('/pick_your_order')
-def pick_your_order():
-    return render_template()         
+@app.route('/make_order', methods=['GET', 'POST'])
+def make_order():
+    if request.method =="POST" :
+        name = request.form['dish_name']
+        size = request.form['size']
+        amount = request.form['amount']
+        connect= db.get_db()
+        connect.execute(f""" INSERT INTO orders (dish_name, size, amount)
+                             VALUES('{name}','{size}','{amount}');""")
+        connect.commit()
+        db.close_db(connect)
+    return render_template('MakeOrder.html')       
 
 
 @app.route('/payment')
